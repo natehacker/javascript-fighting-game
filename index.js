37df3f7cca32c5ec4030371ca6a8166d3665e6a9
Nathan louissaint <louissaintmark@gmail.com>
2023-04-05 22:24:59 -0400

    charaters created in the shape of blocks

--- index.js
+++ index.js
@@ -6,29 +6,65 @@ canvas.height =576
 c.fillRect(0,0,canvas.width,canvas.height)
 
 
+const gravity = 0.2
+
 class Sprite {
-    constructor(position){
+    constructor({position, velcoity}){
         this.position = position
+        this.velcoity = velcoity
+        this.height = 150
     }
     draw(){
         c.fillStyle ="red"
-        c.fillRect(this.position.x,this.position.y,50,150)
+        c.fillRect(this.position.x,this.position.y,50,this.height)
+    }
+    update(){
+        this.draw()
+        this.position.x += this.velcoity.x
+        this.position.y += this.velcoity.y
+      
+        if (this.position.y + this.height + this.velcoity.y >= canvas.height){
+            this.velcoity.y = 0
+        } else this.velcoity.y += gravity
     }
 }
 const player = new Sprite({
+    position:{
     x:0,
     y:0
+},
+velcoity:{
+    x: 0,
+    y:10
+}
 })
-player.draw()
+
 const enemy = new Sprite({
+    position:{
     x:400,
     y:100
+},
+velcoity:{
+    x: 0,
+    y:0
+}
 })
-enemy.draw()
+
 console.log(player)
 
 function animate (){
     window.requestAnimationFrame(animate)
-    console.log("go")
+    c.fillStyle = "black"
+    c.fillRect(0,0,canvas.width, canvas.height)
+player.update()
+enemy.update()
 }
 animate()
+window.addEventListener('keydown',(event)=>{
+    switch(event.key){
+        case 'd':
+        player.velcoity.x = 1
+        break
+    }
+console.log(event)
+})
